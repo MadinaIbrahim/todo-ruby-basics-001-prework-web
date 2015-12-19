f9257622425d3e4e2692ca282cb40c385ac189d0
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2015-12-19 15:28:27 -0500

    Done

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,35 @@
 def division(num1, num2)
- 
+ num1/num2
+
 end
 
-def assign_variable(value)
+def assign_variable(value)#assign_variable should take an argument
+  value="Bob"# of a persons name and assign it to a variable name
+
+end
+
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting(greeting, name) #greeting should take in two arguments
+    
+  puts greeting # should raise an error with one argument
 
 end
 
-def return_a_value
+def return_a_value(phrase="Nice")
+  return phrase
 
 end
 
-def last_evaluated_value
+def last_evaluated_value(phrase="expert")
 
+return phrase
 end
 
-def pizza_party
+def pizza_party(x="cheese")
+return x
 
 end
